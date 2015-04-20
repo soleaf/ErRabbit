ac70836108f3cc1b902d35306bbc6f5678a4a422
soleaf <soleaf@gmail.com>
2015-04-20 19:25:23 +0900

    loading js error

--- src/main/webapp/pages/js/common.js
+++ src/main/webapp/pages/js/common.js
@@ -2,16 +2,19 @@
  * Created by soleaf on 2015. 4. 19..
  */
 function showLoading(){
-    loading = "<div class='loading_box' id='loading'>" +
-    "<div>loading....</div>" +
-    "</div>";
 
-    $("BODY").append(loading).fadeIn(100);
+    if ($("#loading") == null){
+        loading = "<div class='loading_box' id='loading'>" +
+        "<div>loading....</div>" +
+        "</div>";
+        $("BODY").append(loading);
+    }
+
+    $("#loading").fadeIn(100);
+
 }
 
 function hideLoading(){
-    $("#loading").fadeOut(function(){
-        $("#loading").remove();
-    },50);
+    $("#loading").fadeOut(100);
 }
 
