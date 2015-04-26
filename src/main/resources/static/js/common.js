20ee29794f791b772782b16faceb7a097b3d211b
soleaf <soleaf@gmail.com>
2015-04-26 23:26:27 +0900

    fix not appearing loading view bug

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -2,14 +2,6 @@
  * Created by soleaf on 2015. 4. 19..
  */
 function showLoading(){
-
-    if ($("#loading") == null){
-        loading = "<div class='loading_box' id='loading'>" +
-        "<div>loading....</div>" +
-        "</div>";
-        $("BODY").append(loading);
-    }
-
     $("#loading").fadeIn(100);
 
 }
