3d789447eb5aa5ff84ee085e943cb295cffe2a93
soleaf <soleaf@gmail.com>
2015-04-26 23:26:13 +0900

    Move Admin info to properties

--- src/main/java/org/mintcode/errabbit/WebSecurityConfig.java
+++ src/main/java/org/mintcode/errabbit/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package org.mintcode.errabbit;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${errabbit.security.admin.username}")
+    private String username;
+
+    @Value("${errabbit.security.admin.password}")
+    private String password;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -32,6 +40,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+                .withUser(username).password(password).roles("USER");
     }
 }
