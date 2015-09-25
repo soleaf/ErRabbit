26d3adf7a4422b789e5425b410b77bdb426ff425
soleaf <soleaf@gmail.com>
2015-09-25 11:59:24 +0900

    enable webscoket security

--- src/main/java/org/mintcode/errabbit/WebSocketSecurityConfig.java
+++ src/main/java/org/mintcode/errabbit/WebSocketSecurityConfig.java
@@ -1,5 +1,6 @@
 package org.mintcode.errabbit;
 
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 
@@ -8,10 +9,12 @@ import org.springframework.security.config.annotation.web.socket.AbstractSecurit
  * Created by soleaf on 8/1/15.
  */
 
+@Configurable
 public class WebSocketSecurityConfig extends
         AbstractSecurityWebSocketMessageBrokerConfigurer {
     protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
         messages
-                .simpDestMatchers("/topic/console").hasRole("USER");
+                .simpDestMatchers("/topic/console").authenticated();
+
     }
 }
